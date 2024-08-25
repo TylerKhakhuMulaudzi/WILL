0f68d7ebccd8bd089d88edaaf29b366b467eade2
ST10173943 <ST10173943@rcconnect.edu.za>
2024-08-25 11:05:03 +0000

    4AUG2024

--- app/src/main/java/com/example/secondsemster/SizeAdapter.kt
+++ app/src/main/java/com/example/secondsemster/SizeAdapter.kt
@@ -11,29 +11,25 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.secondsemster.databinding.ViewholderBrandBinding
 import com.example.secondsemster.databinding.ViewholderColorBinding
+import com.example.secondsemster.databinding.ViewholderSizeBinding
 
-class ColorAdapter(val items:MutableList<String>):
-    RecyclerView.Adapter<ColorAdapter.Viewholder>() {
+class SizeAdapter(val items:MutableList<String>):
+    RecyclerView.Adapter<SizeAdapter.Viewholder>() {
         private var selectedPosition =-1
         private var lastSelectedPosition = -1
     private lateinit var context : Context
-    inner class Viewholder(val binding:ViewholderColorBinding):
+    inner class Viewholder(val binding:ViewholderSizeBinding):
         RecyclerView.ViewHolder(binding.root) {
-
     }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ColorAdapter.Viewholder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SizeAdapter.Viewholder {
         context = parent.context
-        val binding = ViewholderColorBinding.inflate(LayoutInflater.from(context),parent,false)
+        val binding = ViewholderSizeBinding.inflate(LayoutInflater.from(context),parent,false)
         return Viewholder(binding)
     }
 
-    override fun onBindViewHolder(holder: ColorAdapter.Viewholder, @SuppressLint("RecyclerView") position: Int) {
+    override fun onBindViewHolder(holder: SizeAdapter.Viewholder, @SuppressLint("RecyclerView") position: Int) {
 
-
-        Glide.with(holder.itemView.context)
-            .load(items[position])
-            .into(holder.binding.pic)
+        holder.binding.sizeTxt.text=items[position]
 
         holder.binding.root.setOnClickListener{
             lastSelectedPosition = selectedPosition
@@ -44,9 +40,12 @@ class ColorAdapter(val items:MutableList<String>):
 
         if (selectedPosition == position){
             holder.binding.colorLayout.setBackgroundColor(R.drawable.grey_bg_selected)
+            holder.binding.sizeTxt.setTextColor(context.resources.getColor(R.color.purple))
 
         }else{
             holder.binding.colorLayout.setBackgroundResource(R.drawable.grey_bg)
+            holder.binding.sizeTxt.setTextColor(context.resources.getColor(R.color.black))
+
 
         }
     }
