1fe64bea07722f858c512c2984a48995e3ee2db4
ST10173943 <ST10173943@rcconnect.edu.za>
2024-10-29 19:59:55 +0000

    27Oct2024

--- app/src/main/java/com/example/secondsemster/TinyDB.java
+++ app/src/main/java/com/example/secondsemster/TinyDB.java
@@ -43,7 +43,6 @@ import java.util.Map;
 
 
 public class TinyDB {
-
     private SharedPreferences preferences;
     private String DEFAULT_APP_IMAGEDATA_DIRECTORY;
     private String lastImagePath = "";
