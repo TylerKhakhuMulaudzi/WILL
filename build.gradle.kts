b1fcec60e8f3d3fb0acb64c28a4f9293346a56c3
ST10173943 <ST10173943@rcconnect.edu.za>
2024-11-18 19:27:16 +0000

    18Nov2024

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,18 @@
 buildscript {
     dependencies {
         classpath("com.google.gms:google-services:4.4.2")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
     }
 }
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
 }
 
 allprojects {
-    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
