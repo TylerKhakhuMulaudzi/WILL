abd39ddf93e4f0ffba321bafc3137de561fa5a93
ST10173943 <ST10173943@rcconnect.edu.za>
2024-11-18 19:31:14 +0000

    18Nov2024

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
     id("com.google.firebase.crashlytics")
-    id("com.google.firebase.crashlytics")
 }
 
 android {
@@ -30,13 +29,21 @@ android {
     }
 
     lint {
-        checkDependencies = true
         abortOnError = false
+        checkAllWarnings = true
+        checkReleaseBuilds = false
+        ignoreWarnings = false
+        textReport = true
         htmlReport = true
-        xmlReport = true
+        xmlReport = false
+        sarifReport = true
         baseline = file("lint-baseline.xml")
-        disable += setOf("ObsoleteLintCustomCheck", "GradleDependency")
-        warningsAsErrors = false
+        disable.addAll(setOf(
+            "ObsoleteLintCustomCheck",
+            "GradleDependency",
+            "Typos",
+            "UnusedIds"
+        ))
     }
 
     compileOptions {
@@ -54,55 +61,46 @@ android {
 
     packaging {
         resources {
-            pickFirst("META-INF/NOTICE.md")
-            pickFirst("META-INF/LICENSE.md")
-            pickFirst("META-INF/DEPENDENCIES")
-            pickFirst("META-INF/NOTICE")
-            pickFirst("META-INF/LICENSE")
+            excludes.add("META-INF/DEPENDENCIES")
+            excludes.add("META-INF/LICENSE")
+            excludes.add("META-INF/LICENSE.txt")
+            excludes.add("META-INF/license.txt")
+            excludes.add("META-INF/NOTICE")
+            excludes.add("META-INF/NOTICE.txt")
+            excludes.add("META-INF/notice.txt")
+            excludes.add("META-INF/*.kotlin_module")
         }
     }
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-auth:22.3.1")
-    implementation("com.google.firebase:firebase-database:20.3.0")
-    implementation("com.google.firebase:firebase-storage:20.3.0")
-    implementation("com.google.firebase:firebase-messaging:23.4.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-    implementation("com.github.bumptech.glide:glide:4.16.0")
-    implementation("com.google.firebase:firebase-firestore:24.10.2")
-    implementation("androidx.activity:activity:1.9.3")
-    implementation("com.google.firebase:firebase-functions:21.0.0")
-    implementation("com.google.firebase:firebase-inappmessaging-display")
+    
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-database-ktx")
-    implementation("com.google.firebase:firebase-firestore-ktx:25.1.1")
-    implementation("com.google.firebase:firebase-storage-ktx:21.0.1")
-    implementation("com.google.code.gson:gson:2.11.0")
-    implementation("com.tbuonomo:dotsindicator:5.0")
-    implementation("androidx.viewpager2:viewpager2:1.0.0")
-    implementation("com.google.ai.client.generativeai:common:0.9.0")
-    implementation("com.sun.mail:android-mail:1.6.7")
-    implementation("com.sun.mail:android-activation:1.6.7")
-    implementation("com.google.firebase:firebase-database:20.2.1")
-    implementation("org.jlleitschuh.gradle:ktlint-gradle:11.3.2")
-    implementation("io.gitlab.arturbosch.detekt:detekt-cli:1.23.5")
-    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-    implementation("androidx.activity:activity-ktx:1.4.0")
-
+    implementation("com.google.firebase:firebase-storage-ktx")
+    implementation("com.google.firebase:firebase-messaging-ktx")
+    implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation("com.google.firebase:firebase-crashlytics-ktx")
+    
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    
+    // Image Loading
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    
+    // Testing
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.mockito:mockito-core:4.0.0")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
+    testImplementation("org.mockito:mockito-core:5.10.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
     testImplementation("androidx.arch.core:core-testing:2.2.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
